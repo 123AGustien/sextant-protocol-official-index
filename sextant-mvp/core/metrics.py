60f10213ac79e8148e35c4b3a6210e4fe45b7232
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-22 01:15:26 +0700

    Remove total_nodes parameter from baseline_score

--- sextant-mvp/core/metrics.py
+++ sextant-mvp/core/metrics.py
@@ -1,5 +1,5 @@
 class Metrics:
-    def baseline_score(self, baseline_result, total_nodes):
+    def baseline_score(self, baseline_result):
         return round(baseline_result["success_rate"], 3)
 
     def cascade_impact(self, cascade_result, total_nodes):
@@ -7,5 +7,4 @@ class Metrics:
         return round(failed / total_nodes, 3)
 
     def improvement(self, baseline_score, cascade_impact):
-        # lower is better for cascade impact, higher is better for success
         return round(baseline_score - cascade_impact, 3)
